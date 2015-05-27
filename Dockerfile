c45f2011ca668386021d24359ed62d27fa6bfcea
Sebastian Marconi <smarconi@siu.edu.ar>
2015-05-27 15:26:07 -0300

    Se mueve la carpeta *instalacion* de toba hacia una subcarpeta docker-data/instalacion, para poder meterlo dentro de un unico volumen persistible

--- Dockerfile
+++ Dockerfile
@@ -6,5 +6,8 @@ COPY toba_editor.sh /entrypoint.d/
 ENV JASPER_HOST jasper
 ENV JASPER_PORT 8081
 
+#En lugar de utilizar http://localhost:puerto usa http://ip_interna, para poder hacer comunicaciones internas server-to-server
+#RUN echo "UseCanonicalName On" >> /etc/apache2/apache2.conf
+
 RUN chmod +x /entrypoint.d/*.sh
 
--- toba_editor.sh
+++ toba_editor.sh
@@ -16,24 +16,24 @@ if [ -z "$TOBA_NOMBRE_INSTALACION" ]; then
 fi
 
 HOME_TOBA=/var/local/toba
-
-if [ -z "$(ls -A "$HOME_TOBA/instalacion")" ]; then
+export TOBA_INSTALACION_DIR=${HOME_TOBA}/docker-data/instalacion
+if [ -z "$(ls -A "$TOBA_INSTALACION_DIR")" ]; then
     echo -n postgres > /tmp/clave_pg;
     echo -n ${TOBA_PASS} > /tmp/clave_toba;
     ${HOME_TOBA}/bin/instalar -d ${TOBA_ID_DESARROLLADOR} -t 0 -h pg -p 5432 -u postgres -b toba -c /tmp/clave_pg -k /tmp/clave_toba -n ${TOBA_NOMBRE_INSTALACION}
     
     #Permite a Toba guardar los logs
-	chown -R www-data ${HOME_TOBA}/instalacion/i__desarrollo
+	chown -R www-data ${TOBA_INSTALACION_DIR}/i__desarrollo
 
     #Permite al usuario HOST editar los archivos
-	chmod -R a+w ${HOME_TOBA}/instalacion
+	chmod -R a+w ${TOBA_INSTALACION_DIR}
 fi
 
-ln -s ${HOME_TOBA}/instalacion/toba.conf /etc/apache2/sites-enabled/toba.conf;
+ln -s ${TOBA_INSTALACION_DIR}/toba.conf /etc/apache2/sites-enabled/toba.conf;
 
 #Se deja el ID del container dentro de la configuración de toba, para luego poder usarlo desde el Host
 DOCKER_CONTAINER_ID=`cat /proc/self/cgroup | grep -o  -e "docker-.*.scope" | head -n 1 | sed "s/docker-\(.*\).scope/\\1/"`
-echo "TOBA_DOCKER_ID=$DOCKER_CONTAINER_ID" > ${HOME_TOBA}/instalacion/toba_docker.env
+echo "TOBA_DOCKER_ID=$DOCKER_CONTAINER_ID" > ${TOBA_INSTALACION_DIR}/toba_docker.env
 
 #Cada vez que se loguea por bash al container, carga las variables de entorno toba
 echo ". ${HOME_TOBA}/bin/entorno_toba_trunk.sh" > /root/.bashrc
